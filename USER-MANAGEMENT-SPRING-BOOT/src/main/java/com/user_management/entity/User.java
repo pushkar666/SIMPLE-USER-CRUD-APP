914706067bce81f4cef8633084bfca36744ba203
PUSHKAR D <pushkardwarkanath@gmail.com>
2025-01-18 00:24:05 +0530

    commit-exception-entity_validation

--- USER-MANAGEMENT-SPRING-BOOT/src/main/java/com/user_management/entity/User.java
+++ USER-MANAGEMENT-SPRING-BOOT/src/main/java/com/user_management/entity/User.java
@@ -6,6 +6,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
@@ -18,6 +20,7 @@ public class User {
 	private Integer id;
 
 	@Column(unique = true, nullable = false) // Enforce uniqueness for username
+	@NotBlank(message = "Username is required")
 	private String userName;
 
 	@Column(nullable = false)
@@ -27,6 +30,7 @@ public class User {
 	private String lastName;
 
 	@Column(unique = true) // Consider making email unique if applicable
+	@Email(message = "Invalid email format")
 	private String email;
 
 	@Column(nullable = false)
