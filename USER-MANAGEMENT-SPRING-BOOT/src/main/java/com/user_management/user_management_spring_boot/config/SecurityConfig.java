e94f20a3bc4588d2469f5d2387a3e90d667186f6
PUSHKAR D <pushkardwarkanath@gmail.com>
2025-01-20 10:20:48 +0530

    commit-modification

--- USER-MANAGEMENT-SPRING-BOOT/src/main/java/com/user_management/user_management_spring_boot/config/SecurityConfig.java
+++ USER-MANAGEMENT-SPRING-BOOT/src/main/java/com/user_management/user_management_spring_boot/config/SecurityConfig.java
@@ -66,8 +66,8 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable()) // Disable CSRF for stateless APIs
                 .authorizeHttpRequests(auth -> auth
-                        // .requestMatchers("/auth/welcome", "/auth/addNewUser",
-                        // "/auth/generateToken").permitAll()
+                        .requestMatchers( "/auth/addNewUser",
+                        "/auth/generateToken").permitAll()
                         // .requestMatchers("/auth/user/**").hasAuthority("ROLE_USER")
                         // .requestMatchers("/auth/admin/**").hasAuthority("ROLE_ADMIN")
                         .requestMatchers("/auth/users/**", "/auth/queryUsers/**")
